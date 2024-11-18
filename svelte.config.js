372db21bda232fee38aa75c3831bc0842f40f954
dhavaldivtech <dhaval.divtech@gmail.com>
2024-11-18 16:26:53 +0530

    package install

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: vercel()
+		// Add other configurations if needed
 	}
 };
-
-export default config;
